8364df5732d1e6ce7bd1b3cf9055fdda89fa9502
Tianzhe <wtz1997@126.com>
2020-10-02 21:29:08 +0800

    Init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='multiobj-rationale',
+    version='https://github.com/wengong-jin/multiobj-rationale',
+    packages=find_packages(),
+    url='',
+    license='MIT',
+    author='Wengong Jin',
+    author_email='',
+    description='Multi-Objective Molecule Generation using Interpretable Substructures'
+)
